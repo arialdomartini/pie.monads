2726c05c36e4e2fc6ba59b90ffdd8fe77d006943
Arialdo Martini <arialdomartini@gmail.com>
2018-12-18 17:46:14 +0100

    Unit

---
+++ Monads/Unit.cs
@@ -0,0 +1,10 @@
+﻿namespace Monads
+{
+    public struct Unit
+    {
+        public static Unit Build() => default(Unit);
+
+        // ReSharper disable once InconsistentNaming
+        public static Unit unit => Build();
+    }
+}
---
+++ MonadsTest/UnitTest.cs
@@ -0,0 +1,29 @@
+﻿using FluentAssertions;
+using Xunit;
+using static Monads.Unit;
+using Unit = Monads.Unit;
+
+// ReSharper disable ArrangeStaticMemberQualifier
+namespace MonadsTest
+{
+    public class UnitTest
+    {
+        [Fact]
+        public void there_should_be_only_one_instance_of_unit()
+        {
+            var a = Unit.Build();
+            var b = Unit.Build();
+
+            a.Should().Be(b);
+        }
+
+        [Fact]
+        public void it_should_possible_to_create_a_Unit_with_one_single_word()
+        {
+            var a = unit;
+            var b = Unit.Build();
+
+            a.Should().Be(b);
+        }
+    }
+}
